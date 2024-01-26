f571a913d0c32299faa955c9e4bbbcdbb7b39389
tamar3252 <89608149+tamar3252@users.noreply.github.com>
2024-01-26 16:12:49 +0200

    Update Users.router.ts

--- src/Users/Users.router.ts
+++ src/Users/Users.router.ts
@@ -6,6 +6,7 @@ const routerUser = expressUser.Router();
 
 routerUser.post("/login", userCtrl.login)
 routerUser.post("/signup", userCtrl.signup)
+routerUser.get("/get_user_details",authUser, userCtrl.getUserDetails)
 routerUser.patch("/add_follower/:follow_id",authUser,userCtrl.addFollower)
 routerUser.patch("/remove_follower/:follow_id",authUser,userCtrl.removeFollower)
 routerUser.patch("/change_to_manager",authUser,userCtrl.changeToManager)
